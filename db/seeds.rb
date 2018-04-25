87df2e26de4d66530e6c2fdc15c1655fa178531b
Mihail-K <mihail@platterz.ca>
2018-04-25 19:39:12 -0400

    Add a seed for a test user.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the `rails db:seed` command (or created alongside the database with `db:setup`).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Here's a test User account. You'll be able to login with these credentials.
+# You can replace the email with your own Platterz email, if you so choose.
+User.create!(email: 'test@platterz.ca', password: 'password')
