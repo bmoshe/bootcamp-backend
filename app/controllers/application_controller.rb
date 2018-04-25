d1a79e38538116ad4e71b40051d4d6bc746913f7
Mihail-K <mihail@platterz.ca>
2018-04-25 19:03:16 -0400

    Add authentication logic.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  def current_user
+    current_session&.user
+  end
+
+  def current_session
+    return @current_session if defined?(@current_session)
+    @current_session = Session.find_by(token: session_token)
+  end
+
+private
+
+  def session_token
+    request.headers['Session-Token']
+  end
 end
