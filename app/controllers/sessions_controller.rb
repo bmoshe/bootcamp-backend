36836128a3d834ead5ae3cc69a395d4122f1f265
Mihail-K <mihail@platterz.ca>
2018-04-25 19:44:55 -0400

    Include endpoints.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,20 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  # GET http://localhost:3000/session
   def show
     authorize(Session)
     render json: current_session
   end
 
+  # POST http://localhost:3000/session
   def create
     authorize(Session)
     @session = Session.create!(permitted_attributes(Session))
     render json: @session, status: :created
   end
 
+  # DELETE http://localhost:3000/session
   def destroy
     authorize(Session)
     current_session.destroy!
