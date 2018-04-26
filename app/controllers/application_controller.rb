0c2cfb324a8af9c6bb3e7fe5daba26db8976a746
Mihail-K <mihail@platterz.ca>
2018-04-26 15:26:36 -0400

    Add error handling.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@
 class ApplicationController < ActionController::API
   include Pundit
 
+  # Here we catch errors commonly thrown by our controllers, and gracefully inform the client of their transgressions.
+  # More specifically, authorization errors from Pundit are translated into their respective HTTP headers,
+  # and ActiveRecord errors rendered into something that can be displayed to the user.
+
+  rescue_from(Pundit::NotAuthorizedError) do
+    head current_user.nil? ? :unauthorized : :forbidden
+  end
+
+  rescue_from(ActiveRecord::RecordInvalid, ActiveRecord::RecordNotSaved, ActiveRecord::RecordNotDestroyed) do |error|
+    render json: { errors: [error.record.errors] }, status: :unprocessable_entity
+  end
+
   # This is the app's authentication logic. It tries to fetch an active Session using a secure token.
   # The app expects a session token to be passed in the `Session-Token` header.
 
